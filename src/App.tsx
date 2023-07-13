24ad7df88bba8d90efe07d14e8cb4e1033673544
Anukul Pandey <anukul.030601@gmail.com>
2023-07-13 16:24:25 +0530

    feat: no account instruction

--- src/App.tsx
+++ src/App.tsx
@@ -349,7 +349,23 @@ const App = (): JSX.Element => {
                 Download for Chrome
               </a>
               </div>
-              </div>:<>No account selected</>}
+              </div>:<div> 
+              <div className='no-ext-banner'>
+              <div className="no-ext-headline">
+              Create an Account
+              </div>
+              <br />
+              Use Reef Chain Extension to create your account and refresh the page.
+              </div>
+              <div className='no-ext-imgs'>
+                <img src="/4.png" className='no-ext-img' alt="" />
+                <img src="/5.png" className='no-ext-img' alt="" />
+              </div>
+              <div className='no-ext-imgs'>
+              <img src="/6.png" className='no-ext-img' alt="" />
+                <img src="/7.png" className='no-ext-img' alt="" />
+              </div>
+              </div>}
             </div>
           )}
         </div>
