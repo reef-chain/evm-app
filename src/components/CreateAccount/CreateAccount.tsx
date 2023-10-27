68fd63f155f99215bb2eec9aa9d4fe8e1db543bc
Anukul Pandey <anukul.030601@gmail.com>
2023-10-27 15:26:04 +0530

    fix: extensions not detecting

---
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,23 @@
+function CreateAccount() {
+  return (
+    <div> 
+              <div className='no-ext-banner'>
+              <div className="no-ext-headline">
+              Create an Account
+              </div>
+              <br />
+              Use Reef Chain Extension to create your account and refresh the page.
+              </div>
+              <div className='no-ext-imgs'>
+                <img src="/4.png" className='no-ext-img' alt="" />
+                <img src="/5.png" className='no-ext-img' alt="" />
+              </div>
+              <div className='no-ext-imgs'>
+              <img src="/6.png" className='no-ext-img' alt="" />
+                <img src="/7.png" className='no-ext-img' alt="" />
+              </div>
+              </div>
+  )
+}
+
+export default CreateAccount
